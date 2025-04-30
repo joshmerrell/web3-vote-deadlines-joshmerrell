9ecfa6b026ee696b2cfc2e3bfd26e1b1472fbbdb
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2025-04-30 18:46:57 +0000

    Migrate to US state voting info, start modifying style

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+interface State {
+    label: string;
+    value: string;
+    deadline: string;
+    primaryDeadline: string;
+    primaryDate: string;
+    url: string;
+    onlineAccepted: boolean;
+    lastMinuteAccepted: boolean;
+    emoji: string;
+}
