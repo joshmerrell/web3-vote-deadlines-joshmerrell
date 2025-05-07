40e2ab40bbd4779841bf101726bbfa03624a2b41
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2025-05-07 17:27:05 +0000

    Move states, header, and footer to separate components, create layout

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+export interface State {
+    label: string;
+    value: string;
+    deadline: string;
+    primaryDeadline: string;
+    primaryDate: string;
+    url: string;
+    onlineAccepted: boolean;
+    lastMinuteAccepted: boolean;
+    emoji: string;
+}
